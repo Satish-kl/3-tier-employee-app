6f6e5e3fe887a15fedccdd3cec40acbe4911fca1
satish kumar <satish@kumar.com>
2026-08-19 23:27:28 +0530

    Update API URL for EC2 deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 
-const URL = "http://localhost:3000";
+const URL = "http://35.173.29.47:3000";
 
 function App() {
   const [data, setData] = useState([]);
